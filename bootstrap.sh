91d16489e710b2b3948c36f254e7dcead30bf175
Carl Allen <github@allenofmn.com>
2024-01-16 17:02:54 -0600

    Fix lumina, on-my-zsh and bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 xcode-select --install
 
-/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
